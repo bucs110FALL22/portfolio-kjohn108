7eab98b10c889e1945acb3def1f7d9b93a71a1b8
KJohnson108 <112440903+kjohn108@users.noreply.github.com>
2022-09-24 00:48:40 +0000

    lab 3

--- ch03/lab/main.py
+++ ch03/lab/main.py
@@ -1,5 +1,8 @@
 import turtle #1. import modules
 import random
+import pygame
+import math 
+pygame.init()
 
 #Part A
 window = turtle.Screen() # 2.  Create a screen
@@ -18,9 +21,33 @@ michelangelo.goto(-100,20)
 leonardo.goto(-100,-20)
 
 ## 5. Your PART A code goes here
-
-
+for i in range(10): 
+  leonardo.forward(random.randrange(0,10))
+  michelangelo.forward(random.randrange(0,10))
+pygame.time.wait(2000)
 # PART B - complete part B here
+
+window = pygame.display.set_mode()
+pygame.Color((0,0,255))
+for i in range(5):
+  coords = []
+  num_sides = int(input("how many sides?"))
+  sidelength = int(input("how long are the sides?"))
+  offset = int(input("how far from top corner?"))
+  theta = (2.0 * math.pi * i) / num_sides
+  
+  for a in range(num_sides):
+    theta = (2.0 * math.pi * i) / num_sides
+    x = sidelength * math.cos(theta) + offset
+    y = sidelength * math.sin(theta) + offset
+    coords.append([x,y])
+  pygame.draw.polygon
+shape= pygame.draw.polygon(window, "white", [coords])
+print(shape)
+pygame.display.flip()
+pygame.time.wait(500)
+window.fill
+
 
 
 window.exitonclick()
