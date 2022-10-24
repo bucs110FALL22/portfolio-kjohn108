fa8ca90f3a02ef25b622c70e22757992f40c04a4
KJohnson108 <112440903+kjohn108@users.noreply.github.com>
2022-10-24 21:54:00 +0000

    gui classes

---
+++ ch06/exercises/gui_classes.py
@@ -0,0 +1,17 @@
+class player:
+  def __init__(self):
+    self.heath = 10 #start with full health
+    self.speed = 1 
+
+
+class scoreboard: 
+  def __init__(scoreboard):
+    scoreboard.score = 0 
+    scoreboard.color = white
+    scoreboard.font = mario
+
+class mushroom:
+  def __init__(mushroom):
+    mushroom.health = 5
+    mushroom.speed = 2
+    mushroom.size = normal
